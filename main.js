24a5847e8861746b44cc461a85f8172718437d8f
fortym2 <30431538+fortym2@users.noreply.github.com>
2020-04-14 11:57:57 +0200

    Add CSV parser and DOM elements creation

--- main.js
+++ main.js
@@ -10,8 +10,37 @@ const loadCSV = async() => {
 
 loadConfig().then(loadCSV).then(computeData);
 
+let rowsNumber = 0;
+
 function computeData() {
     data = document.querySelector('#data');
     
     document.querySelector('header h1').innerHTML = config.projectName;
+    
+    const lines = csv.split('\n');
+    lines.forEach(line => {
+        const cells = line.split(',');
+        const item = cells[0];
+        const events = [];
+        for (i=1; i<cells.length; i++) {
+            const attributes = cells[i].split('|');
+            const date = attributes[0];
+            const type = attributes[1];
+            events.push({date, type})
+        }
+        createRow(item, events);
+    })
+}
+
+function createRow(item, events) {
+    let currentRowNumber = ++rowsNumber;
+    data.innerHTML += `<div class="row" id="row-${currentRowNumber}"><div class="name">${item}</div><div class="events"></div></div>`;
+    let eventsDiv = document.querySelector(`.row#row-${currentRowNumber} .events`)
+    events.forEach(event => {
+        eventsDiv.innerHTML += 
+            `<div class="event type-${event.type}">
+                <div class="type">${event.type}</div>
+                <div class="date">${event.date}</div>
+            </div>`
+    });
 }
