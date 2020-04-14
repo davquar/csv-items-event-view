9e4f982ed935d7277ed8a04077b5fa81e147c249
fortym2 <30431538+fortym2@users.noreply.github.com>
2020-04-14 13:30:00 +0200

    Add options for CSV parsing - delimitator - skip empty rows

--- main.js
+++ main.js
@@ -23,15 +23,17 @@ function computeData() {
     
     const lines = csv.split('\n');
     lines.forEach(line => {
-        const cells = line.split(',');
+        const cells = line.split(config.delimitator);
         const item = cells[0];
         const events = [];
         for (i=1; i<cells.length; i++) {
+            if (cells[i] == '') continue;
             const attributes = cells[i].split('|');
             const date = attributes[0];
             const type = attributes[1];
             events.push({date, type})
         }
+        if (config.skipEmptyRows && events.length == 0) return;
         createRow(item, events);
     })
 
@@ -43,9 +45,10 @@ function createRow(item, events) {
     data.innerHTML += `<div class="row" id="row-${currentRowNumber}"><div class="name">${item}</div><div class="events"></div></div>`;
     let eventsDiv = document.querySelector(`.row#row-${currentRowNumber} .events`)
     events.forEach(event => {
+        const mappedType = config.eventTypeMapping[event.type];
         eventsDiv.innerHTML += 
             `<div class="event type-${event.type}">
-                <div class="type">${config.eventTypeMapping[event.type]}</div>
+                <div class="type">${mappedType == undefined ? event.type : mappedType}</div>
                 <div class="date">${event.date}</div>
             </div>`
     });
